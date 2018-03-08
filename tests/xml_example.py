284f6231dea1a503a296d19a80f6944a61d4753e
Hay Kranen <huskyr@gmail.com>
2018-03-08 14:31:02 +0100

    Adding xml parsing example

---
+++ tests/xml_example.py
@@ -0,0 +1,24 @@
+from dataknead import Knead, BaseLoader
+import xmltodict
+
+# Add a custom loader for textfiles
+class XmlLoader(BaseLoader):
+    EXTENSION = "xml"
+
+    @staticmethod
+    def read(f):
+        return xmltodict.parse(f.read())
+
+    @staticmethod
+    def write(f, data, pretty = True):
+        xmldata = xmltodict.unparse(data, pretty = pretty)
+        f.write(xmldata)
+
+Knead.loaders.append(XmlLoader)
+
+def parse(data):
+    books = data["catalog"]["book"]
+    data["catalog"]["book"] = [b for b in books if b["author"] == "O'Brien, Tim"]
+    return data
+
+Knead("input/books.xml").apply(parse).write("output/obrien-books.xml")
