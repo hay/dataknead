a7aba32d2da489ad6895f25d6111c126c1740fc3
Hay Kranen <huskyr@gmail.com>
2018-05-26 00:18:17 +0200

    Adding a YAML example

---
+++ tests/yamlloader.py
@@ -0,0 +1,19 @@
+from _context import dataknead
+from dataknead import Knead, BaseLoader
+import yaml
+
+class YamlLoader(BaseLoader):
+    EXTENSION = "yaml"
+
+    @staticmethod
+    def read(f):
+        return yaml.load(f)
+
+    @staticmethod
+    def write(f, data, pretty = True):
+        yamldata = yaml.dump(data)
+        f.write(yamldata)
+
+Knead.loaders.append(YamlLoader)
+
+Knead("input/example.yaml").write("output/example.json", indent = 4)
