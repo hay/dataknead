d72cdc8ad46dff814621ed08884749cc7fdb612a
Hay Kranen <huskyr@gmail.com>
2018-07-02 17:53:50 +0200

    Adding version and copyright information

--- dataknead/__init__.py
+++ dataknead/__init__.py
@@ -1,2 +1,8 @@
+__title__ = 'dataknead'
+__version__ = '0.3.0-beta'
+__author__ = 'Hay Kranen'
+__license__ = 'MIT'
+__copyright__ = 'Copyright Hay Kranen <huskyr@gmail.com>'
+
 from .knead import Knead
 from .baseloader import BaseLoader
