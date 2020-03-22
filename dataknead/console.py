a7752560ca7e067772e4d4cae65a5bd0de1e0b57
Hay Kranen <huskyr@gmail.com>
2020-03-22 01:00:49 +0100

    Removing the removed .query() option from the cli

--- dataknead/console.py
+++ dataknead/console.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from .knead import Knead
-from pathlib import Path
-from os.path import isfile
 from argparse import ArgumentParser
+from os.path import isfile
+from pathlib import Path
 import logging
 import sys
 
@@ -25,9 +25,6 @@ def get_parser():
     )
     parser.add_argument("--input-format", "-if", type=str, help="Input format")
     parser.add_argument("--output-format", "-of", type=str, help="Output format")
-    parser.add_argument("-q", "--query", type=str,
-        help = "Add a jq-compatible query for the loaded data"
-    )
     parser.add_argument(
         "--stdin",
         action="store_true",
@@ -71,9 +68,6 @@ def main(args):
             raise Exception("File does not exist: %s" % args.input)
         else:
             k = Knead(args.input, parse_as=args.input_format)
-
-    if args.query:
-        k = k.query(args.query)
 
     # Okay, now let's do some converting!
     # With no output, just print to stdout
