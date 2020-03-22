d3d8ecb516c43ce9f94795ac9d8b1092eaa30946
Hay Kranen <huskyr@gmail.com>
2020-03-22 22:42:06 +0100

    Removing meta from __init__, because that is now handled by pyproject

--- dataknead/__init__.py
+++ dataknead/__init__.py
@@ -1,8 +1,2 @@
-__title__ = 'dataknead'
-__version__ = '0.3.0'
-__author__ = 'Hay Kranen'
-__license__ = 'MIT'
-__copyright__ = 'Copyright Hay Kranen <huskyr@gmail.com>'
-
 from .knead import Knead
 from .baseloader import BaseLoader
